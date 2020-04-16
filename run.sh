89b8accea3b0b355cffa2070045512b099381909
kusari-k <18eh501@ms.dendai.ac.jp>
2020-04-16 12:23:12 +0900

    remove prerun.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/bash
+if [ ! -e /etc/nginx/user_conf.d/default.conf.old ]; then
+	cp -frp /etc/nginx/default_conf.d/default.conf.old /etc/nginx/user_conf.d/default.conf.old && \
+	cp -frp /etc/nginx/default_conf.d/html /usr/share/nginx/html && \
+	echo $(date)>>/var/log/copy_default.log && \
+	echo "Copy default.conf.old">>/var/log/copy_default.log && \
+	echo >>/var/log/copy_default.log
+fi
+
+chown -R nginx:nginx /etc/nginx/user_conf.d
+chown -R nginx:nginx /usr/share/nginx
 mkdir -p /run/php-fpm/
+
+#start web program
+rsyslogd
+
+nginx -c /etc/nginx/nginx.conf
+
+php-fpm
+
+tail -f /dev/null
--- script.sh
+++ script.sh
@@ -2,15 +2,11 @@
 
 #version test
 version_code="1"
-if [ $(sed -e "s/^##.*//g"  -e "/^$/d" setting.txt|grep version|head -n 1|sed s/.*://) != $version_code ];then
+setting_file_version=$(sed -e "s/^##.*//g"  -e "/^$/d" setting.txt|grep version|head -n 1|sed s/.*://)
+if [ "x$setting_file_version" != "x$version_code" ];then
         echo "setting.txt version error"
         exit 1;
 fi;
-
-#host setting
-mkdir -m 777 -p  /home/docker_home/
-mkdir -p /var/log/docker_log/webserver/nginx/
-mkdir -p /var/log/docker_log/webserver/php-fpm/
 
 #read setting file
 sed -e "s/^##.*//g"  setting.txt |\
@@ -31,6 +27,12 @@ sed -e "s/^##.*//g"  setting.txt |\
 	sed -ze "s/.*-----user data-----//g" \
 	-e "s/-----.*//g" |\
 	sed -e /^$/d>webserver-user.log
+
+#host setting
+mkdir -m 777 -p  /home/docker_home/
+mkdir -p /var/log/docker_log/webserver/nginx/
+mkdir -p /var/log/docker_log/webserver/php-fpm/
+rm *.log
 
 read -p "do you want to up this container ? (y/n):" yn
 if [ ${yn,,} = "y" ]; then
