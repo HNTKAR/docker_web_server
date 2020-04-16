6a2396b0bbda57aa9dca83e3da0caeeea1412b34
kusari-k <18eh501@ms.dendai.ac.jp>
2020-04-17 03:05:45 +0900

    remake script.sh

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,27 @@
 FROM centos
 MAINTAINER kusari-k
 
+EXPOSE 3306
 ARG root_password="password"
 
-RUN curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash && \
-	dnf update -y && \
-	dnf install -y rsyslog MariaDB passwd && \
-	dnf update -y && \
+RUN curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
+RUN sed -i -e "\$afastestmirror=true" /etc/dnf/dnf.conf
+RUN dnf update -y && \
+	dnf install -y rsyslog MariaDB && \
 	dnf clean all
 
-#EXPOSE 25 995 993 465 587
-
-COPY run.sh  /usr/local/bin/
-
-RUN  chmod 755 /usr/local/bin/run.sh
-
+#/usr/bin/mysql_secure_installation
 RUN sed -i.old -e "/read\ / s/.*/echo/ " \
 	-e "/trap/a password1=$root_password" \
 	-e "/trap/a password2=$root_password" /usr/bin/mysql_secure_installation
 
+#/etc/rsyslog.conf
+RUN sed -i -e "/imjournal/ s/^/#/" \
+	-e "s/off/on/" /etc/rsyslog.conf
+
 RUN cp -frp /var/lib/mysql /var/lib/mysql_default
+
+COPY run.sh  /usr/local/bin/
+RUN  chmod 755 /usr/local/bin/run.sh
 
 ENTRYPOINT ["/usr/local/bin/run.sh"]
--- script.sh
+++ script.sh
@@ -8,33 +8,47 @@ if [ "x$setting_file_version" != "x$version_code" ];then
         exit 1;
 fi;
 
+cd $(dirname $0)
+
 #read setting file
-sed -e "s/^##.*//g"  setting.txt |\
+sed -e "s/^##.*//g"  setting.txt | \
        	sed -ze "s/.*=====general=====//g" \
-	-e  "s/=====.*//g" |\
+	-e  "s/=====.*//g" | \
 	sed -e /^$/d>general.log
 
-sed -e "s/^##.*//g"  setting.txt |\
+sed -e "s/^##.*//g"  setting.txt | \
 	sed -ze "s/.*=====database=====//g" \
-	-e  "s/=====.*//g" |\
+	-e  "s/=====.*//g" | \
 	sed -ze "s/.*-----system data-----//g" \
-	-e "s/-----.*//g" |\
-	sed -e /^$/d>database-system.log
+	-e "s/-----.*//g" | \
+	sed -e /^$/d>system.log
 
-sed -e "s/^##.*//g"  setting.txt |\
+sed -e "s/^##.*//g"  setting.txt | \
 	sed -ze "s/.*=====database=====//g" \
-	-e  "s/=====.*//g" |\
+	-e  "s/=====.*//g" | \
 	sed -ze "s/.*-----user data-----//g" \
-	-e "s/-----.*//g" |\
-	sed -e /^$/d>database-user.log
+	-e "s/-----.*//g" | \
+	sed -e /^$/d>user.log
 
+#host setting
 mkdir -m 777 -p  /home/docker_home/
 
 #set docker-compose.yml
-sed -i -e "/root_password/ s/:.*/:\ $(grep root_password database-system.log | sed s/.*://)/" docker-compose.yml
+sed -i -e "/root_password/ s/:.*/:\ $(grep root_password system.log | sed s/.*://)/" docker-compose.yml
 
-rm *.log
 read -p "do you want to up this container ? (y/n):" yn
 if [ ${yn,,} = "y" ]; then
 	docker-compose up --build -d
+	rm -fr remove_firewall.sh
+
+	grep access_ip system.log | \
+		sed -e "s/.*://" | \
+		xargs -I {} \
+		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER 1 ! -s {} -p tcp --dport 3306 -j DROP
+	grep access_ip system.log | \
+		sed -e "s/.*://" | \
+		xargs -I {} \
+		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER 1 ! -s {} -p tcp --dport 3306 -j DROP">>remove_firewall.sh && \
+		chmod 775 remove_firewall.sh
 fi
+rm *.log
