838bb84f4ab3ed22f97ef2a1eb1169804659b242
kusari-k <18eh501@ms.dendai.ac.jp>
2020-04-18 02:48:24 +0900

    tweak

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ ! -e /etc/nginx/user_conf.d/default.conf.old ]; then
 	cp -frp /etc/nginx/default_conf.d/default.conf.old /etc/nginx/user_conf.d/default.conf.old && \
 	cp -frp /etc/nginx/default_conf.d/html /usr/share/nginx/html && \
 	echo $(date)>>/var/log/copy_default.log && \
-	echo "Copy /etc/nginx/default_conf.d/default.conf.old from /etc/nginx/user_conf.d/default.conf.old ">>/var/log/copy_default.log && \
+	echo "Copy /etc/nginx/default_conf.d/default.conf.old from /etc/nginx/user_conf.d/default.conf.old">>/var/log/copy_default.log && \
 	echo >>/var/log/copy_default.log
 fi
 
