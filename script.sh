701f328924d306a5d3ec2d4f036d1bb8c6c59baf
kusari-k <18eh501@ms.dendai.ac.jp>
2020-04-18 00:51:14 +0900

    rename

--- script.sh
+++ script.sh
@@ -40,22 +40,29 @@ if [ ${yn,,} = "y" ]; then
 	docker-compose up --build -d
 	rm -fr remove_firewall.sh
 
-	grep access_ip system.log | \
-		sed -e "s/.*://" | \
-		xargs -I {} \
-		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER 1 ! -s {} -p tcp --dport 80 -j DROP
-	grep access_ip system.log | \
-		sed -e "s/.*://" | \
-		xargs -I {} \
-		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER 1 ! -s {} -p tcp --dport 443 -j DROP
-	grep access_ip system.log | \
-		sed -e "s/.*://" | \
-		xargs -I {} \
-		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER 1 ! -s {} -p tcp --dport 80 -j DROP">>remove_firewall.sh && \
-	grep access_ip system.log | \
-		sed -e "s/.*://" | \
-		xargs -I {} \
-		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER 1 ! -s {} -p tcp --dport 443 -j DROP">>remove_firewall.sh && \
+	firewall_opt=$(grep firewall_block system.log | sed -e "s/.*://")
+	if [ x${firewall_opt,,} = "xon" ]; then
+		firewall-cmd --direct --add-chain ipv4 filter DOCKER-USER-WEB && \
+		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER 1 -j DOCKER-USER-WEB && \
+		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER-WEB 99 -p tcp --dport 80 -j DROP && \
+		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER-WEB 99 -p tcp --dport 443 -j DROP && \
+		grep access_ip system.log | \
+			sed -e "s/.*://" | \
+			xargs -I {} firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER-WEB 1 -s {} -p tcp --dport 80 -j ACCEPT && \
+		grep access_ip system.log | \
+			sed -e "s/.*://" | \
+			xargs -I {} firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER-WEB 1 -s {} -p tcp --dport 443 -j ACCEPT && \
+		grep access_ip system.log | \
+			sed -e "s/.*://" | \
+			xargs -I {} echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER-WEB 1 -s {} -p tcp --dport 80 -j ACCEPT">>remove_firewall.sh && \
+		grep access_ip system.log | \
+			sed -e "s/.*://" | \
+			xargs -I {} echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER-WEB 1 -s {} -p tcp --dport 443 -j ACCEPT">>remove_firewall.sh && \
+		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER-WEB 99 -p tcp --dport 80 -j DROP">>remove_firewall.sh && \
+		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER-WEB 99 -p tcp --dport 443 -j DROP">>remove_firewall.sh && \
+		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER 1 -j DOCKER-USER-WEB">>remove_firewall.sh && \
+		echo "firewall-cmd --direct --remove-chain ipv4 filter DOCKER-USER-WEB">>remove_firewall.sh && \
 		chmod 775 remove_firewall.sh
+	fi
 fi
 rm *.log
