51541a12b7baebb65fbba91f150f8165db0481eb
kusari-k <45356209+kusari-k@users.noreply.github.com>
2021-02-23 23:48:47 +0900

    use php8.0

---
+++ run-nginx.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+#start rsyslog
+rsyslogd
+
+
+#if [ ! -e /etc/nginx/user_conf.d/default.conf.old ]; then
+#	cp -frp /etc/nginx/default_conf.d/default.conf.old /etc/nginx/user_conf.d/default.conf.old && \
+#	cp -frp /etc/nginx/default_conf.d/html /usr/share/nginx/html && \
+#	echo $(date)>>/var/log/copy_default.log && \
+#	echo "Copy /etc/nginx/default_conf.d/default.conf.old from /etc/nginx/user_conf.d/default.conf.old">>/var/log/copy_default.log && \
+#	echo >>/var/log/copy_default.log
+#fi
+
+#chown -R nginx:nginx /etc/nginx/user_conf.d
+#chown -R nginx:nginx /usr/share/nginx
+
+#start nginx
+#nginx -c /etc/nginx/nginx.conf
+
+#mkdir -p /run/php-fpm/
+
+#start FastCGI
+#php-fpm
+
+tail -f /dev/null
--- script.sh
+++ script.sh
@@ -1,68 +1,20 @@
 #!/bin/bash
 
-#version test
-version_code="1"
-setting_file_version=$(sed -e "s/^##.*//g"  -e "/^$/d" setting.txt|grep version|head -n 1|sed s/.*://)
-if [ "x$setting_file_version" != "x$version_code" ];then
-        echo "setting.txt version error"
-        exit 1;
-fi;
-
 cd $(dirname $0)
 
 #read setting file
-sed -e "s/^##.*//g"  setting.txt | \
-       	sed -ze "s/.*=====general=====//g" \
-	-e  "s/=====.*//g" | \
-	sed -e /^$/d>general.log
+sed -z -e "s/.*##\+web#*//g" \
+	-e "s/##.\+//g" setting.txt >setting.log
+export web_DOMAIN=$(grep ^web_domain setting.log|sed "s/.*://")
+export SSL_DOMAIN=$(grep ^ssl_domain setting.log|sed "s/.*://")
 
-sed -e "s/^##.*//g"  setting.txt | \
-	sed -ze "s/.*=====webserver=====//g" \
-	-e  "s/=====.*//g" | \
-	sed -ze "s/.*-----system data-----//g" \
-	-e "s/-----.*//g" | \
-	sed -e /^$/d>system.log
+cp -frp /home/podman/ssl_pod/letsencrypt .
 
-sed -e "s/^##.*//g"  setting.txt | \
-	sed -ze "s/.*=====webserver=====//g" \
-	-e  "s/=====.*//g" | \
-	sed -ze "s/.*-----user data-----//g" \
-	-e "s/-----.*//g" | \
-	sed -e /^$/d>user.log
-
-#host setting
-mkdir -m 777 -p  /home/docker_home/
-mkdir -p /var/log/docker_log/webserver/nginx/
-mkdir -p /var/log/docker_log/webserver/php-fpm/
-
+#build image
 read -p "do you want to up this container ? (y/n):" yn
 if [ ${yn,,} = "y" ]; then
-	docker-compose up --build -d
-	rm -fr remove_firewall.sh
-
-	firewall_opt=$(grep firewall_block system.log | sed -e "s/.*://")
-	if [ x${firewall_opt,,} = "xon" ]; then
-		firewall-cmd --direct --add-chain ipv4 filter DOCKER-USER-WEB && \
-		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER 1 -j DOCKER-USER-WEB && \
-		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER-WEB 99 -p tcp --dport 80 -j DROP && \
-		firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER-WEB 99 -p tcp --dport 443 -j DROP && \
-		grep access_ip system.log | \
-			sed -e "s/.*://" | \
-			xargs -I {} firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER-WEB 1 -s {} -p tcp --dport 80 -j ACCEPT && \
-		grep access_ip system.log | \
-			sed -e "s/.*://" | \
-			xargs -I {} firewall-cmd --direct --add-rule ipv4 filter DOCKER-USER-WEB 1 -s {} -p tcp --dport 443 -j ACCEPT && \
-		grep access_ip system.log | \
-			sed -e "s/.*://" | \
-			xargs -I {} echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER-WEB 1 -s {} -p tcp --dport 80 -j ACCEPT">>remove_firewall.sh && \
-		grep access_ip system.log | \
-			sed -e "s/.*://" | \
-			xargs -I {} echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER-WEB 1 -s {} -p tcp --dport 443 -j ACCEPT">>remove_firewall.sh && \
-		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER-WEB 99 -p tcp --dport 80 -j DROP">>remove_firewall.sh && \
-		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER-WEB 99 -p tcp --dport 443 -j DROP">>remove_firewall.sh && \
-		echo "firewall-cmd --direct --remove-rule ipv4 filter DOCKER-USER 1 -j DOCKER-USER-WEB">>remove_firewall.sh && \
-		echo "firewall-cmd --direct --remove-chain ipv4 filter DOCKER-USER-WEB">>remove_firewall.sh && \
-		chmod 775 remove_firewall.sh
-	fi
+	podman rmi -f nginx
+	podman rmi -f mariadb-master
+	podman build -f Dockerfile-nginx -t nginx:latest --build-arg WEB_DOMAIN=$WEB_DOMAIN --build-arg SSL_DOMAIN=$SSL_DOMAIN
 fi
 rm *.log
