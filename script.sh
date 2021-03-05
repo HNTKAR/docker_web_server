22a0231859da3363c7cd44349e6e579a901eba54
kusari-k <45356209+kusari-k@users.noreply.github.com>
2021-03-05 23:41:33 +0900

    fix README.md

--- script.sh
+++ script.sh
@@ -11,10 +11,11 @@ cp -frp /home/podman/ssl_pod/letsencrypt .
 #build image
 read -p "do you want to up this container ? (y/n):" yn
 if [ ${yn,,} = "y" ]; then
-#	podman rmi -f nginx
-	podman rmi -f php
-	podman rmi -f mariadb
-#	podman build -f Dockerfile-nginx -t nginx:latest
+	podman rmi -f nginx
+	podman rmi -f php-fpm
+	podman rmi -f mariaDB
+	podman build -f Dockerfile-nginx -t nginx:latest
 	podman build -f Dockerfile-php-fpm -t php-fpm:latest
+	podman build -f Dockerfile-mariaDB -t mariaDB:latest
 fi
 rm *.log
